f1220c1f929548d5af2e7b34432baf70539de075
Jeremy <jncameron1@hotmail.com>
2023-12-31 18:58:54 +1100

    Another form test

--- src/components/forms/ContactFormTest.vue
+++ src/components/forms/ContactFormTest.vue
@@ -1,36 +1,28 @@
 <template>
-<form class ="contact-form" name="contact-form" method="post" 
-							data-netlify="true"
-							data-netlify-honeypot="bot-field">
-						<div class="form-field">
-							<label>Name</label>
-							<input type="text" name="Name" placeholder="Name" required>
-							</div>
-						<div class="form-field">
-							<label>Email</label>
-							<input type="email" name="email" placeholder="Email Address" required>
-							</div>
+<form
+  name="contact-form"
+  method="POST"
+  data-netlify="true"
+  @submit.prevent="handleSubmit">
+  <input type="hidden" name="form-name" value="contact-form" />
 
-						<div class="form-field">
-							<label>Message</label>
-							<textarea name="message" rows="3" placeholder="Message (optional)"></textarea>
-							</div>
-						
-						<div class="form-field">		
-							<label>Budget</label>	
-							<textarea name="budget" rows="0" placeholder="Budget (optional)"></textarea>
-							</div>
-						<div class="form-submit">	
-							<button class="contact-button" type="submit" name="submit" >Enquire Now</button>
-							</div>
-					</form>	
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
 </template>
 
 <script setup>
-document
-  .querySelector("form")
-  .addEventListener("submit", handleSubmit);
-  
 const handleSubmit = (event) => {
   event.preventDefault();
 
